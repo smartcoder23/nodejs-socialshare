49a736de269b993e6c6c787a1b9e59b14088d71b
Shubham Sethi <shubhamsethikcm@gmail.com>
2018-10-27 13:17:27 +0530

    dfds

---
+++ app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var fs=require('fs');
+
+/* connect to database */
+var mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost/dbphotogallery1');
+// mongodb://<dbuser>:<dbpassword>@ds119449.mlab.com:19449/dbphotogallery1
+mongoose.connect('mongodb://shubhamsethi:cssecure123@ds119449.mlab.com:19449/dbphotogallery1')
+.then(() =>  console.log('connection succesful'))
+.catch((err) => console.error(err));
+require("./models/Photo");
+/* Define routes */
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+// app.engine('html',function(path,opt,fn){
+//      fs.readFile(path,'utf-8',function(err,str){
+//        if(err)return str;
+//     else return fn(null,str);
+//     })
+//    })
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+app.listen(8000);
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router    = express.Router();
+var upload    = require('./upload');
+var mongoose  = require('mongoose');
+var Photo     = mongoose.model('Photos');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  Photo.find({}, ['path','caption'], {sort:{ _id: -1} }, function(err, photos) {
+    res.render('index', { title: 'NodeJS file upload tutorial', msg:req.query.msg, photolist : photos });
+    
+  });
+
+});
+
+/** Upload file to path and add record to database */
+
+router.post('/upload', function(req, res) {
+
+  upload(req, res,(error) => {
+      if(error){
+         res.redirect('/?msg=3');
+      }else{
+        if(req.file == undefined){
+          
+          res.redirect('/?msg=2');
+
+        }else{
+             
+            /**
+             * Create new record in mongoDB
+             */
+            var fullPath = "files/"+req.file.filename;
+
+            var document = {
+              path:     fullPath, 
+              caption:   req.body.caption
+            };
+  
+          var photo = new Photo(document); 
+          photo.save(function(error,result){
+            if(error){ 
+              throw error;
+            } 
+            if(result!=null){
+              console.log("+++++++++++++++++",result)
+              var id={}
+              id=result._id;
+              console.log("id agaye",id)
+            res.render('index.pug',{id:id});
+         }});
+      }
+    }
+  });    
+});
+
+router.post('/socialshare/:id',function(req,res)
+{
+ // Let's check user-agents to see if this is a social bot. If so, let's serve a different layout to populate the og data so it looks pretty when sharing.
+     console.log("ssassa",req);
+      Photo.findOne({_id: req.params.id }, function(err, results) {
+          if(err) {
+
+               return err;
+          } else if (results !== null) {
+            
+              console.log("hello",results)
+              var newData = {};
+                
+                  newData.socialTitle=results.caption,
+                  
+                  newData.socialImage=""
+                  newData.socialUrl= '' + req.url
+                  res.render('index.pug', newData );
+                }})})
+module.exports = router;
