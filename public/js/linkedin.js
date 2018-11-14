4414b136362d8da7bdf949b444f6448c576290d8
smartcoder23 <35736555+smartcoder23@users.noreply.github.com>
2018-11-14 11:40:10 +0530

    Update linkedin.js

--- public/js/linkedin.js
+++ public/js/linkedin.js
@@ -1,7 +1,10 @@
-
-function onLinkedInLoad(){
-  IN.Event.on(IN, "auth", shareContent);
-}
+// IN.Event.on(IN, "liauth", shareContent);
+// function onLinkedInLoad(){
+//   console.log("hello")
+//   // shareContent()
+//      IN.Event.on(IN,"AQWPcp4HREdNtgG89lEJSo9i_Y0N2KK9p_HHClnziHD7STNRTCTtxwBg1zzz1jlKhrWP4hwPekV-NtwDiK76CPFA7En_fjcKHfpAm3qm59YXGRhD3bBIFiPyOAFbDfsDslcCi9eNQbTmX86698GpO_VyFBWhjYNp0zyqEd4m01TrXjRrijXbNrSx7eaZup0CCOze4Gr268LXG3sBmA3Hc6OLHrbqRZpS3-5YY58-6bMZq-LNCJKse_EmBPbuIRbWPMBCor9DorRviOF2jqugU139vijsapqS7aidHU7ukrwCX5jf1ke6BNYquBnn2FX5iout-Q7pC_5OqekyDR9g7xWZ5Sk2CA", shareContent);
+    
+//  }
 
 // Handle the successful return from the API call
 function onSuccess(data) {
@@ -24,9 +27,19 @@ var payload = {
   } 
 };
 
+
 IN.API.Raw("/people/~/shares?format=json")
   .method("POST")
   .body(JSON.stringify(payload))
   .result(onSuccess)
   .error(onError);
 }
+
+function liAuth(){
+  console.log("dfdss");
+  IN.UI.Authorize().params({"scope":["r_basicprofile", "r_emailaddress","w_share"]}).place();
+  IN.Event.on(IN, 'auth', shareContent);
+//   IN.User.authorize(function(){
+// onLinkedInLoad();
+//   });
+  }
